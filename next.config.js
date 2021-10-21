ca3ce9a7b5becc413a9522a90c066db4f59c600a
Cas Guzdziol <cguzdziol@uwm.com>
2021-10-21 18:03:06 -0400

    configure for styled-components for Next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withTM = require('next-transpile-modules')([
+  '@mui/material',
+  '@mui/system',
+])
+module.exports = withTM({
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@mui/styled-engine': '@mui-styled-engine-sc',
+    }
+    return config
+  },
+})
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui-styled-engine-sc',
+    },
+  },
 }
